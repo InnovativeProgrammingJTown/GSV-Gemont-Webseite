db3d13325b37ccc1d2d2d64ee9da8954b4c18488
mfreyipj <matthiasfrey.ipj@gmail.com>
2019-07-15 15:29:09 +0200

    Initialisierung des Kalenders (noch nur über Desktop erreichbar)

--- JS/StartJS.js
+++ JS/StartJS.js
@@ -11,37 +11,53 @@
    newsSlider.style.transform = 'translate(-50%)';
  });
 
-// Image Slider Startseite
+ // Image Slider Startseite
 
- const left = document.querySelector('.left');
- const right = document.querySelector('.right');
+  const left = document.querySelector('.left');
+  const right = document.querySelector('.right');
 
- const slider = document.querySelector('.slider');
+  const slider = document.querySelector('.slider');
 
- const indicatorParent = document.querySelector('.control ul');
- const indicators = document.querySelectorAll('.control li');
- index = 0;
+  const indicatorParent = document.querySelector('.control ul');
+  const indicators = document.querySelectorAll('.control li');
+  index = 0;
 
- indicators.forEach((indicator, i) => {
-   indicator.addEventListener('click', () => {
+  indicators.forEach((indicator, i) => {
+    indicator.addEventListener('click', () => {
+      document.querySelector('.control .selected').classList.remove('selected');
+      indicator.classList.add('selected');
+      slider.style.transform = 'translateX(' + (i) * -25 + '%)';
+      index = i;
+
+    })
+ });
+
+ var intervalID = 0;
+
+ function startShow(sec){
+   intervalid = setInterval(function(){
+     index = (index < 4 - 1) ? index+1 : 0;
      document.querySelector('.control .selected').classList.remove('selected');
-     indicator.classList.add('selected');
-     slider.style.transform = 'translateX(' + (i) * -25 + '%)';
-     index = i;
+     indicatorParent.children[index].classList.add('selected');
+     slider.style.transform = 'translateX(' + (index) * -100 + '%)';
+   }, sec);
+ }
+ startShow(8000);
 
-   })
-});
+  left.addEventListener('click', function() {
+    index = (index > 0) ? index -1 : 3;
+    document.querySelector('.control .selected').classList.remove('selected');
+    indicatorParent.children[index].classList.add('selected');
+    slider.style.transform = 'translateX(' + (index) * -100 + '%)';
+    clearInterval(intervalID);
 
- left.addEventListener('click', function() {
-   index = (index > 0) ? index -1 : 0;
-   document.querySelector('.control .selected').classList.remove('selected');
-   indicatorParent.children[index].classList.add('selected');
-   slider.style.transform = 'translateX(' + (index) * -100 + '%)';
- });
+  });
 
- right.addEventListener('click', function() {
- index = (index < 4 - 1) ? index+1 : 3;
- document.querySelector('.control .selected').classList.remove('selected');
- indicatorParent.children[index].classList.add('selected');
- slider.style.transform = 'translateX(' + (index) * -100 + '%)';
- });
+  right.addEventListener('click', function() {
+  index = (index < 4 - 1) ? index+1 : 0;
+  document.querySelector('.control .selected').classList.remove('selected');
+  indicatorParent.children[index].classList.add('selected');
+  slider.style.transform = 'translateX(' + (index) * -100 + '%)';
+  clearInterval(intervalID);
+
+  });
